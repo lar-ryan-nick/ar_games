d703c6f9fef82824ed43e30df591891e80f5747d
Zakary Bliss <zakarybliss@ucsb.edu>
2020-05-28 15:20:24 -0700

    Changed computers card to no target, add pot on the side for war, and added card counter.

--- public/main.js
+++ public/main.js
@@ -9,8 +9,10 @@ AFRAME.registerComponent('main', {
         this.playerText = document.getElementById('player-text')
         this.deck = new Array();
         this.players = new Array();
+        this.warStatus = false;
+        this.warPot = new Array()
 
-				this.createDeck = this.createDeck.bind(this)
+        this.createDeck = this.createDeck.bind(this)
 				this.createPlayers = this.createPlayers.bind(this)
 				this.shuffle = this.shuffle.bind(this)
 				this.startGame = this.startGame.bind(this)
@@ -19,11 +21,19 @@ AFRAME.registerComponent('main', {
 				this.checkForWinner = this.checkForWinner.bind(this)
 				this.clearCards = this.clearCards.bind(this)
 				this.placeCard = this.placeCard.bind(this)
+                this.addCardCounter = this.addCardCounter.bind(this)
 
 				const playerTappable = document.getElementById('player-tappable')
 				playerTappable.addEventListener('click', function(event) {
-					this.flipCards();
+				    this.playerText.setAttribute("visible", "false")
+
+                    if(!this.warStatus) {
+                        this.flipCards();
+                    } else {
+                        this.startWar();
+                    }
 				}.bind(this))
+
         this.startGame()
     },
 
@@ -32,7 +42,7 @@ AFRAME.registerComponent('main', {
             for (var x = 0; x < suits.length; x++) {
                 var weight = parseInt(values[i]);
                 if (values[i] == "J")
-                    weight = 10;
+                    weight = 11;
                 if (values[i] == "Q")
                     weight = 12;
                 if (values[i] == "K")
@@ -96,21 +106,21 @@ AFRAME.registerComponent('main', {
     flipCards: function() {
 				this.clearCards(this.playerTarget)
 				this.clearCards(this.computerTarget)
+        this.addCardCounter()
 
         playersCard = this.players[0].Hand.shift();
         computersCard = this.players[1].Hand.shift();
 
         console.log(playersCard)
 
-				this.placeCard(this.playerTarget, playersCard, 0, false);
-				this.placeCard(this.computerTarget, computersCard, 0, false);
+				this.placeCard(true, playersCard, 0, false, false);
+				this.placeCard(false, computersCard, 0, false, false);
 
         if (playersCard.Weight == computersCard.Weight) {
             // If cards are the same rank begin a war and add the two cards to the pot
-            var warPot = new Array()
-            warPot.push(playersCard)
-            warPot.push(computersCard)
-            this.startWar(warPot)
+            this.warStatus = true
+            this.warPot.push(computersCard)
+            this.warPot.push(playersCard)
         } else if (playersCard.Weight > computersCard.Weight) {
             // If the player has a higher rank card, add both cards to the bottom of their hand
             this.players[0].Hand.push(playersCard)
@@ -126,16 +136,31 @@ AFRAME.registerComponent('main', {
 				console.log(this.players[1].Hand)
     },
 
-		placeCard: function(target, card, offset, back) {
-        const image = document.createElement('a-image')
-        image.object3D.scale.set(0.75, 0.75, 0.75)
-        image.object3D.position.set(0, offset, offset)
-				if (!back) {
-					image.setAttribute('src', "#" + card.Suit + "_" + card.Value.toLowerCase())
-				} else {
-					image.setAttribute('src', "#back")
-				}
-        target.appendChild(image)
+		placeCard: function(playerTarget, card, offset, back, pot) {
+            const image = document.createElement('a-image')
+            image.object3D.scale.set(0.75, 0.75, 0.75);
+            var xOffset = 0;
+            var yOffset = 0;
+            var zOffset = offset;
+
+            if(pot) {
+                xOffset = .75
+                yOffset = -.4
+            }
+            if(!playerTarget) {
+                yOffset = -.8
+            } else {
+                yOffset = yOffset + offset
+            }
+
+            image.object3D.position.set(xOffset, yOffset, zOffset)
+
+            if (!back) {
+                image.setAttribute('src', "#" + card.Suit + "_" + card.Value.toLowerCase())
+            } else {
+                image.setAttribute('src', "#back")
+            }
+            this.playerTarget.appendChild(image)
 		},
 
 		clearCards: function(target) {
@@ -145,7 +170,12 @@ AFRAME.registerComponent('main', {
 			})
 		},
 
-    startWar: function(warPot) {
+    startWar: function() {
+        this.clearCards(this.playerTarget)
+        this.clearCards(this.computerTarget)
+        this.addCardCounter()
+        //console.log("hand length before: " + this.players[0].Hand.length)
+
         // Check if both players have enough cards for the war
         if (this.players[0].Hand.length < 3) {
             // Computer wins
@@ -155,30 +185,43 @@ AFRAME.registerComponent('main', {
             // Player wins
 						this.playerText.setAttribute('value', 'Opponent ran out of cards for war. You won!')
         }
+                //this.placeCard(true, this.warPot.top, 0, true, true)
 
-				for (var i = 0; i < 3; ++i) {
-					var playersCard = this.players[0].Hand.shift();
-					var computersCard = this.players[1].Hand.shift();
-					warPot.push(playersCard)
-					warPot.push(computersCard)
-					this.placeCard(this.playerTarget, playersCard, warPot.length / 2 * 0.1, i != 2);
-					this.placeCard(this.computerTarget, computersCard, warPot.length / 2 * 0.1, i != 2);
+                for (var i = 0; i < 2; ++i) {
+                    var playersCard = this.players[0].Hand.shift();
+                    var computersCard = this.players[1].Hand.shift();
+                    this.warPot.push(playersCard)
+                    this.warPot.push(computersCard)
+                }
+
+				for (var i = 0; i < this.warPot.length; ++i) {
+					this.placeCard(true, null, i / 2 * 0.1, true, true);
 				}
 
+                var playersCard = this.players[0].Hand.shift();
+                var computersCard = this.players[1].Hand.shift();
+				this.placeCard(true, playersCard, 0, false, false);
+                this.placeCard(false, computersCard, 0, false, false);
+
 				console.log('WAR!')
-				console.log(warPot)
+				console.log(this.warPot)
 
         if (playersCard.Weight == computersCard.Weight) {
             // If cards are the same rank continue the war and add the two cards to the pot
-            this.startWar(warPot)
         } else if (playersCard.Weight > computersCard.Weight) {
             // If the player has a higher rank card, add the pot to the bottom of their hand
-            this.players[0].Hand.push.apply(this.players[0].Hand, warPot)
+            this.players[0].Hand.push.apply(this.players[0].Hand, this.warPot)
             this.checkForWinner()
+            this.warStatus = false;
+            this.warPot = new Array();
+            //console.log("hand length after: " + this.players[0].Hand.length)
         } else {
             // If the computer has a higher rank card, add the pot to the bottom of their hand
-            this.players[1].Hand.push.apply(this.players[1].Hand, warPot)
+            this.players[1].Hand.push.apply(this.players[1].Hand, this.warPot)
             this.checkForWinner()
+            this.warStatus = false;
+            this.warPot = new Array();
+            //console.log("hand length after: " + this.players[0].Hand.length)
         }
     },
 
@@ -191,5 +234,25 @@ AFRAME.registerComponent('main', {
             //TODO: display computer win
 						this.playerText.setAttribute('value', 'You lost.')
         }
+    },
+
+    addCardCounter: function () {
+        this.cardCounter = document.createElement('a-image')
+        this.cardCounter.setAttribute("src", "#back")
+        this.cardCounter.setAttribute("scale", ".25 .25")
+        this.cardCounter.setAttribute("position", "-.5 .6 0")
+
+        this.ccText = document.createElement("a-text")
+        this.ccText.setAttribute("rotation", "0 0 180")
+        this.ccText.setAttribute("width", "10")
+        this.ccText.setAttribute("height", "10")
+        this.ccText.setAttribute("position", "0 0 0" )
+        this.ccText.setAttribute("color", "black")
+        this.ccText.setAttribute("align", "center")
+        this.ccText.setAttribute("value", "-1")
+
+        this.cardCounter.appendChild(this.ccText)
+        this.ccText.setAttribute('value', this.players[0].Hand.length.toString())
+        this.playerTarget.appendChild(this.cardCounter)
     }
 });
